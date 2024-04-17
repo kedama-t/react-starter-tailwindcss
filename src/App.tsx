3599869bc1b31b7d61781793b38ead5a568d55e7
kedama-t <114888622+kedama-t@users.noreply.github.com>
2024-04-17 11:08:51 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <>
+      <div className="flex flex-col gap-2 text-center bg-slate-200 rounded-xl w-11/12 m-auto mt-4">
+        <h1 className="text-3xl font-bold underline">
+          React Starter with TailwindCSS
+        </h1>
+        <p>Hello world!</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
